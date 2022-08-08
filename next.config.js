42d6d79a8fbc8c1c6554b1acb11b0ebfc60749dc
Matheus Costa <matheus.costa@poli.digital>
2022-08-08 07:56:04 -0300

    Remove redirect

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   swcMinify: true,
+
   experimental: {
     runtime: 'experimental-edge',
-  },
-  redirects: async () => {
-    return [
-      {
-        source: '/',
-        destination: 'https://www.lostark.party',
-        permanent: true,
-      },
-    ]
   },
 }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,13 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const slug = req.nextUrl.pathname.split("/").pop();
 
   const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+
+  console.log(slugFetch)
+
   if (slugFetch.status === 404) {
     return NextResponse.redirect(req.nextUrl.origin);
   }
+
   const data = await slugFetch.json();
 
   if (data?.redirect) {
