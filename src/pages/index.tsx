5e83b98868a83cd076f2f7c79f5f073fc6e893bf
Matheus Costa <matheus.costa@poli.digital>
2022-08-15 13:31:11 -0300

    Update redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
 import type { GetServerSideProps, NextPage } from 'next'
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: process.env.BASE_APP_URL!,
-      permanent: false,
-    },
-  }
-}
-
 const Home: NextPage = () => {
   return (
     <div>
