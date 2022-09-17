85cd583b1a9cc48a2b9d0d4cc53ebbd8dbb29776
Matheus Costa <matheus.costa@poli.digital>
2022-09-17 09:09:02 -0300

    add cors to del-url

--- src/pages/api/del-url/[slug].ts
+++ src/pages/api/del-url/[slug].ts
@@ -1,9 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 import { deleteUrl } from "../../../lib/redis";
 
 const get = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  await NextCors(req, res, {
+    methods: ['GET'],
+    origin: '*',
+  })
+
   const slug = req.query["slug"];
+
 
   if (!slug || typeof slug !== "string") {
     res.statusCode = 404;
